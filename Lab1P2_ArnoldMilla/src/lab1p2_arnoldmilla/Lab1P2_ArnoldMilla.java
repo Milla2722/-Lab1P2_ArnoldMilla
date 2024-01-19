f77c12d0ccad1a5b7607a6ae2d27e9a7041d57a1
Milla2722 <147879029+Milla2722@users.noreply.github.com>
2024-01-19 13:34:29 -0600

    Add files via upload

---
+++ Lab1P2_ArnoldMilla/src/lab1p2_arnoldmilla/Lab1P2_ArnoldMilla.java
@@ -0,0 +1,28 @@
+package lab1p2_arnoldmilla;
+import java.util.*;
+
+public class Lab1P2_ArnoldMilla {
+static Scanner sc = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        System.out.println("Ingrese una opcion: ");
+        System.out.println("1. Registrar usuario");
+        System.out.println("2. Listar todo");
+        System.out.println("3. Listar por dominio");
+        int opc = sc.nextInt();
+        switch (opc){
+            case 1:{
+            
+            }break;
+            
+            case 2:{
+            
+            }break;
+            
+            case 3:{
+            
+            }break;
+        }
+    }
+    
+}
