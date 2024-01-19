2edbadef89c1f077dcfc4776d0c835d4a8f01176
Milla2722 <147879029+Milla2722@users.noreply.github.com>
2024-01-19 14:33:00 -0600

    Add files via upload

--- Lab1P2_ArnoldMilla/src/lab1p2_arnoldmilla/Lab1P2_ArnoldMilla.java
+++ Lab1P2_ArnoldMilla/src/lab1p2_arnoldmilla/Lab1P2_ArnoldMilla.java
@@ -1,28 +1,67 @@
 package lab1p2_arnoldmilla;
+import java.time.temporal.TemporalAdjusters;
 import java.util.*;
 
 public class Lab1P2_ArnoldMilla {
 static Scanner sc = new Scanner(System.in);
     
     public static void main(String[] args) {
+        System.out.println("-------Menu-------");
         System.out.println("Ingrese una opcion: ");
         System.out.println("1. Registrar usuario");
         System.out.println("2. Listar todo");
         System.out.println("3. Listar por dominio");
+        System.out.println("4. Salir");
         int opc = sc.nextInt();
-        switch (opc){
-            case 1:{
+        ArrayList <String> registro = new ArrayList<>();
+        while (opc <= 4){
+            switch (opc){
+                case 1:{  
+                    String usuario = "";
+                    System.out.println("Ingrese su nombre");
+                    usuario += sc.next();
+                    usuario += ";";
+                    System.out.println("Ingrese su apellido");
+                    usuario += sc.next();
+                    usuario += ";";
+                    System.out.println("Ingrese su fecha de nacimiento formato dd/MM/yyyy");
+                    String fecha = sc.next();
+                    Date fechaD = new Date(fecha);
+                    int año = fechaD.getYear();
+                    if (año > 111){
+                        System.out.println("Usted es menor de edad");
+                        break;
+                    }                    
+                    else {
+                        usuario += fecha;
+                        usuario += ";";
+                    }
+                    System.out.println("Ingrese su correo electronico");
+                    usuario += sc.next();
+                    usuario += ";";
+                    System.out.println("Ingrese su contraseña");
+                    usuario += sc.next();
+                    usuario += ";";
+                    registro.add(usuario);
+                }break;
             
-            }break;
+                case 2:{
+                    System.out.println(registro);
+                }break;
             
-            case 2:{
+                case 3:{
             
-            }break;
-            
-            case 3:{
-            
-            }break;
+                }break;
+            }
+            System.out.println("-------Menu-------");
+            System.out.println("Ingrese una opcion: ");
+            System.out.println("1. Registrar usuario");
+            System.out.println("2. Listar todo");
+            System.out.println("3. Listar por dominio");
+            System.out.println("4. Salir");
+            opc = sc.nextInt();
         }
+        
     }
     
 }
